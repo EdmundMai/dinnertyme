01b4d93defc99487238e199458ca9bd70b4dc185
Edmund Mai <edmundmai@gmail.com>
2012-11-06 12:44:22 -0500

    making random number integer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
 	def index
-	  offset = rand(Recipe.count)
+	  offset = rand(Recipe.count).to_i
 	  @recipes = Recipe.find(:all, :limit => 3, :order => offset)
 	end
 
