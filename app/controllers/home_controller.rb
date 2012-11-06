c880252e843512420ff6c0b16edb9958999c137f
Edmund Mai <edmundmai@gmail.com>
2012-11-06 14:36:15 -0500

    fixed homepage random integer bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
 	def index
 	  offset = rand(Recipe.count).ceil
-	  @recipes = Recipe.find(:all, :limit => 3, :order => offset)
+	  @recipes = Recipe.find(:all, :limit => 3, :order => "created_at DESC")
 	end
 
 end
